95634ddb77038363b13352df5b64b8df009b652d
Kyungrae Kim <jeremiah.kim@hotmail.com>
2019-10-21 22:50:12 -0700

    Reptile class completed

---
+++ IBuiltAZoo/IBuiltAZoo/Classes/Reptile.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IBuiltAZoo.Classes
+{
+    class Reptile : Animal
+    {
+        public bool HasScale { get; set; }
+        public virtual bool Egg { get; set; } = true;
+        public Reptile(string species, bool extinct, string reproduce, bool scale) : base(species, extinct, reproduce)
+        {
+            HasScale = scale;
+        }
+
+        abstract public void Fly();
+        public virtual void Hunt()
+        {
+            Console.WriteLine("Where is my food?");
+        }
+    }
+}
